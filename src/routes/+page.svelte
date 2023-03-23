485000135a7c6b0a4b2a346be0e3205951701aad
Adam Podkowinski <adampodkdev@gmail.com>
2023-03-23 23:20:26 +0100

    Finish main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,44 @@
 <script lang="ts">
   import Saos from "saos";
   import Icon from "@iconify/svelte";
+  import { fade } from "svelte/transition";
+
+  let jsHovered = false;
+  let flutterHovered = false;
+  const transitionDuration = 300;
+  const jsClass = `row-start-1 col-start-1 row-end-4 col-end-4 text-blue-300/50
+cursor-pointer
+            hover:shadow-sm hover:text-yellow-300
+duration-[300ms]
+            lg:hover:scale-[200%]          lg:origin-top-left          transition-all`;
+  const flutterClass = `row-start-4 col-start-4 row-end-7 col-end-7
+lg:hover:scale-[200%] cursor-pointer
+          duration-[300ms]
+          lg:origin-bottom-right text-blue-300/50 hover:text-blue-500
+          transition-all hover:shadow-sm`;
+  const mobile = (cls: string) => `${cls} lg:hidden`;
+  const desktop = (cls: string) => `${cls} lg:block hidden`;
 </script>
 
 <div class="lg:flex-row flex items-center flex-col px-8 lg:px-40 pb-12">
   <div class="lg:w-[60%]">
-    <Saos
-      once={true}
-      animation={"slide-in-top .4s cubic-bezier(0.35, 0.5, 0.65, 0.95) .4s both"}
-    >
+    <Saos once={true} animation={"slide-in-top .4s cubic-bezier(0.35, 0.5, 0.65, 0.95) .4s both"}>
       <div class="lg:py-12 py-3 text-xl tracking-wide lg:mr-9">
-        <p class="text-6xl font-bold grid py-4 lg:py-10 leading-snug tracking-normal">
+        <p class="lg:text-6xl text-5xl font-bold grid py-4 lg:py-10 leading-tight tracking-normal">
           Hello, It's Adam 👨‍💻
         </p>
         <p>
-          I'm Adam Podkowinski, a full-stack JavaScript developer, ready to design and code your
-          website!
+          I'm Adam Podkowinski, a Mobile (<a
+            href="https://flutter.dev"
+            target="_blank"
+            rel="noreferrer"
+            class="text-blue-500 underline">Flutter</a
+          >) and full-stack JavaScript developer, ready to design and code your website!
         </p>
         <p class="mt-5">
           I strive to make my websites as performant and user-accessible as possible.
         </p>
-        <p class="mb-4">
+        <p>
           Because of that, I take great pleasure in using technologies such as
           <a
             href="https://svelte.dev"
@@ -34,6 +52,8 @@
             rel="noreferrer">Tailwind&nbsp;CSS</a
           >.
         </p>
+        <p class="mt-4">I'm currently a high-school student actively looking for a job.</p>
+        <p>If you're interested, please feel free to contact me!</p>
         <a
           class="font-medium px-6 py-4 text-black rounded-xl mt-8 bg-green-400
           hover:bg-green-500 w-full text-center
@@ -54,12 +74,52 @@
       </div>
     </Saos>
   </div>
-  <div class="lg:w-[40%] w-[30%] mt-6 lg:mt-0">
-    <Saos
-      once={true}
-      animation={"slide-in-right .6s cubic-bezier(0.35, 0.5, 0.65, 0.95) .5s both"}
-    >
-      <Icon icon="teenyicons:javascript-outline" height="100%" />
+  <div class="lg:w-[40%] mt-6 lg:mt-0 relative">
+    <Saos once={true} animation={"slide-in-right .6s cubic-bezier(0.35, 0.5, 0.65, 0.95) .4s both"}>
+      {#if !flutterHovered && !jsHovered}
+        <div
+          class="absolute bottom-[-10%] left-[100%] font-bold text-sm whitespace-nowrap
+      -rotate-45
+      animate-pulse"
+          in:fade={{ duration: transitionDuration }}
+          out:fade={{ duration: transitionDuration }}
+        >
+          ⬆ Click Me
+        </div>
+      {/if}
+      <div
+        class="flex gap-6 w-36 lg:w-auto lg:grid lg:gap-0 relative grid-cols-6
+        grid-rows-6 auto-cols-fr auto-rows-fr"
+      >
+        <div class={mobile(jsClass)}>
+          <Icon icon="simple-icons:javascript" width="100%" />
+        </div>
+        <div class={mobile(flutterClass)}>
+          <Icon icon="simple-icons:flutter" width="100%" />
+        </div>
+        {#if !flutterHovered}
+          <div
+            class={desktop(jsClass)}
+            on:mouseenter={() => (jsHovered = true)}
+            on:mouseleave={() => (jsHovered = false)}
+            out:fade={{ duration: transitionDuration - 100 }}
+            in:fade={{ duration: transitionDuration - 100 }}
+          >
+            <Icon icon="simple-icons:javascript" width="100%" />
+          </div>
+        {/if}
+        {#if !jsHovered}
+          <div
+            class={desktop(flutterClass)}
+            on:mouseenter={() => (flutterHovered = true)}
+            on:mouseleave={() => (flutterHovered = false)}
+            out:fade={{ duration: transitionDuration - 100 }}
+            in:fade={{ duration: transitionDuration - 100 }}
+          >
+            <Icon icon="simple-icons:flutter" width="100%" />
+          </div>
+        {/if}
+      </div>
     </Saos>
   </div>
 </div>
