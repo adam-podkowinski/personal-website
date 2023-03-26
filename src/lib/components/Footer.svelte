6ddcf5f9b3dc1fa7ac6e729b86ee8d753df053ae
Adam Podkowinski <adampodkdev@gmail.com>
2023-03-26 16:33:58 +0200

    Prepare for github pages

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,4 @@
 <footer class="lg:px-40 px-8 bg-slate-800/30 py-9 lg:py-10 shadow-sm shadow-slate-800/40 text-md">
   <p>Adam Podkowiński&copy;</p>
+  <p>Site created with SvelteKit + TailwindCSS</p>
 </footer>
